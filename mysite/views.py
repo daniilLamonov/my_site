bfb893215e7ce591786f17793904f99834f76f50
Daniil Lamonov <lamonovdaniil@icloud.com>
2025-02-05 22:03:33 +0300

    Add paginator

--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,7 @@
 from django.core.mail import EmailMessage
 from django.conf import settings
 from django.contrib import messages
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 from django.shortcuts import render, redirect
 from django.utils import timezone
@@ -8,7 +9,16 @@ from django.utils import timezone
 from mysite.forms import ContactForm
 from mysite.models import Post, Works
 
-
+def paginate(request, obj):
+    paginator = Paginator(obj, 2)
+    page = request.GET.get('page')
+    try:
+        page = paginator.page(page)
+    except PageNotAnInteger:
+        page = paginator.page(1)
+    except EmptyPage:
+        page = paginator.page(paginator.num_pages)
+    return page
 # Create your views here.
 def main(request):
     posts = Post.objects.order_by('-date_posted')[:2]
@@ -17,10 +27,12 @@ def main(request):
 
 def works(request):
     works = Works.objects.filter(date_posted__lte=timezone.now()).order_by('date_posted')
+    works = paginate(request, works)
     return render(request, 'works.html', {'works': works})
 
 def post_list(request):
     posts = Post.objects.filter(date_posted__lte=timezone.now()).order_by('date_posted')
+    posts = paginate(request, posts)
     return render(request, 'post_list.html', {'posts': posts})
 
 def contact(request):
@@ -38,10 +50,8 @@ def contact(request):
                 from_email=email,
                 to=[settings.CONTACT_EMAIL]
             )
-
             if file:
                 email_message.attach(file.name, file.read(), file.content_type)
-
             email_message.send()
             messages.success(request, "Your message has been sent successfully!")
             return redirect('contact')
@@ -49,5 +59,4 @@ def contact(request):
             messages.error(request, "There was an error with your submission. Please try again.")
     else:
         form = ContactForm()
-
         return render(request, 'contact.html', {'form': form})
